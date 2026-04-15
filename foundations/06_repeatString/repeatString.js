8bed7f3d0d07a0a6bce94ed7149e19ebad4ee149
Carter Weinberg <weinbergcarter@gmail.com>
2026-04-15 19:41:12 -0400

    Completed exercise 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (str, amount) {
+    if (amount < 0) {
+        return "ERROR"
+    }
 
+    let repeatedStr = "";
+    let i = 0;
+    while (i < amount) {
+        repeatedStr += str;
+        i++;
+    }
+
+    return repeatedStr;
 };
 
 // Do not edit below this line
