2b6b66efd2cd4f699184783c430f4b357f36f540
Carter Weinberg <weinbergcarter@gmail.com>
2026-04-15 20:18:18 -0400

    Completed exercise 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function (str) {
+    return str.split("").reverse().join("");
 
+
+    function reverse(strArr) {
+        if (strArr.length === 0) {
+            return strArr;
+        } else {
+            const [first, ...rest] = arr;
+            return reverse(rest).concat(first);
+        }
+    }
 };
+
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
