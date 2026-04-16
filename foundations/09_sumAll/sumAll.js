16bc3b1e449ce3d83c750c32cc84e542eed479d4
Carter Weinberg <weinbergcarter@gmail.com>
2026-04-15 21:10:51 -0400

    Completed 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
 
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
+
+
+
+    if (start <= end) {
+        return range(start, end).reduce(((accumulator, currentValue) => accumulator + currentValue), 0);
+    } else {
+        return range(end, start).reduce(((accumulator, currentValue) => accumulator + currentValue), 0);
+    }
 };
+
+
+function range(start, end) {
+    return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+}
 
 // Do not edit below this line
 module.exports = sumAll;
