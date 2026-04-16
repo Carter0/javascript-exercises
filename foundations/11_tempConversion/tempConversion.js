5698d328972190b29ff49c59bff0bbeace006e86
Carter Weinberg <weinbergcarter@gmail.com>
2026-04-15 21:48:21 -0400

    completed 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempF) {
+  // Subtract 32 from the Fahrenheit value
+  // Multiply the result by 5/9
+
+  const tempC = (tempF - 32) * (5 / 9);
+  return Math.round(tempC * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempC) {
+  return Math.round(((tempC * 9) / 5 + 32) * 10) / 10;
+
 };
 
 // Do not edit below this line
