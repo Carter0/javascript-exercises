8e0e7da3eb61bbf56ebaefcffca42ed1b264ffb3
Carter Weinberg <weinbergcarter@gmail.com>
2026-05-05 18:38:58 -0400

    Completed exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const sanitized = string.toLowerCase().replace(/\p{P}/gu, "").replaceAll(" ", "");
+    const arrStr = [...sanitized];
+    const reverseArrStr = arrStr.toReversed();
 
+    return arrStr.toString() === reverseArrStr.toString();
 };
 
 // Do not edit below this line
