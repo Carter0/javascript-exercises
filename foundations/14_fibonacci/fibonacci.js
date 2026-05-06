e8612d2670a1b99782db7cd523b83c70792a3108
Carter Weinberg <weinbergcarter@gmail.com>
2026-05-05 20:09:26 -0400

    Completed fib

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    if (n < 0) {
+        return "OOPS";
+    }
 
+    return fibHelper(Number(n));
 };
+
+
+function fibHelper(n) {
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    } else {
+        return fibHelper(n - 1) + fibHelper(n - 2);
+    }
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
